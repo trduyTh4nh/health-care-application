4d2a75f838c0269f6af128a515376ec7470bb033
fowardslash <quangrain2014@gmail.com>
2024-06-07 15:37:15 +0700

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,121 @@
+import 'package:app_well_mate/screen/home.dart';
 import 'package:app_well_mate/screen/login.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
+  final ColorScheme colorScheme = const ColorScheme(
+      brightness: Brightness.light,
+      primary: Color(0xFF6B94FF),
+      onPrimary: Color(0xFFFFFFFF),
+      secondary: Color(0xFFA8C5FD),
+      onSecondary: Color(0xFF000000),
+      error: Color(0xFFFF7D7D),
+      onError: Color(0xFF000000),
+      surface: Color(0xFFFFFFFF),
+      onSurface: Color(0xFF000000),
+      surfaceDim: Color(0xFFF2F2F2));
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+          colorScheme: colorScheme,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(colorScheme.primary),
+                foregroundColor: WidgetStateProperty.all(colorScheme.onPrimary),
+                shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(16)))),
+                minimumSize: WidgetStateProperty.all(Size(40, 50)),
+                elevation: WidgetStateProperty.resolveWith((state) {
+                  if (state.contains(WidgetState.pressed)) {
+                    return 5;
+                  }
+                  return 0;
+                })),
+          ),
+          filledButtonTheme: FilledButtonThemeData(style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(colorScheme.surfaceDim),
+            foregroundColor: WidgetStateProperty.all(colorScheme.onSurface),
+            minimumSize: WidgetStateProperty.all(Size(40, 50)),
+            shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(16)))),
+          )),
+          textButtonTheme: TextButtonThemeData(style: ButtonStyle(
+            shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(16)))),
+            minimumSize: WidgetStateProperty.all(Size(40, 50)),
+          )),
+          textTheme: TextTheme(
+            bodyLarge: GoogleFonts.inter(
+              fontSize: 16
+            ),
+            bodyMedium: GoogleFonts.inter(
+              fontSize: 14
+            ),
+            bodySmall: GoogleFonts.inter(
+              fontSize: 12
+            ),
+            displayLarge: GoogleFonts.inter(
+              fontSize: 54,
+              fontWeight: FontWeight.bold
+            ),
+            displayMedium: GoogleFonts.inter(
+              fontSize: 46,
+              fontWeight: FontWeight.bold
+            ),
+            displaySmall: GoogleFonts.inter(
+              fontSize: 32,
+              fontWeight: FontWeight.bold
+            ),
+            headlineLarge: GoogleFonts.inter(
+              fontSize: 28,
+              fontWeight: FontWeight.bold
+            ),
+            headlineMedium: GoogleFonts.inter(
+              fontSize: 24,
+            ),
+            headlineSmall: GoogleFonts.inter(
+              fontSize: 20,
+            ),
+            labelLarge: GoogleFonts.inter(
+              fontSize: 16
+            ),
+            labelMedium: GoogleFonts.inter(
+              fontSize: 14
+            ),
+            labelSmall: GoogleFonts.inter(
+              fontSize: 12
+            ),
+            titleLarge: GoogleFonts.inter(
+              fontWeight: FontWeight.bold
+            ),
+            titleMedium: GoogleFonts.inter(
+              fontWeight: FontWeight.bold
+            ),
+            titleSmall: GoogleFonts.inter(
+              fontWeight: FontWeight.bold
+            )
+          ),
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: colorScheme.surface,
+            foregroundColor: colorScheme.onSurface,
+            elevation: 5,
+            iconSize: 32,
+            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(60)))
+          )
+          ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: () {}, child: Icon(Icons.add),),
+        appBar: AppBar(title: Text("Home"), scrolledUnderElevation: 0,),
         body: Center(
-          child: Login(),
+          child: Home(),
         ),
       ),
     );
---
+++ lib/screen/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("bodyLarge", style: Theme.of(context).textTheme.bodyLarge),
+        Text("bodyMedium", style: Theme.of(context).textTheme.bodyMedium),
+        Text("bodySmall", style: Theme.of(context).textTheme.bodySmall),
+        Text("displayLarge", style: Theme.of(context).textTheme.displayLarge),
+        Text("displayMedium", style: Theme.of(context).textTheme.displayMedium),
+        Text("displaySmall", style: Theme.of(context).textTheme.displaySmall),
+        Text("headlineLarge", style: Theme.of(context).textTheme.headlineLarge),
+        Text("headlineMedium", style: Theme.of(context).textTheme.headlineMedium),
+        Text("headlineSmall", style: Theme.of(context).textTheme.headlineSmall),
+        Text("labelLarge", style: Theme.of(context).textTheme.labelLarge),
+        Text("labelMedium", style: Theme.of(context).textTheme.labelMedium),
+        Text("labelSmall", style: Theme.of(context).textTheme.labelSmall),
+        Text("titleLarge - AppBar", style: Theme.of(context).textTheme.titleLarge),
+        Text("titleMedium", style: Theme.of(context).textTheme.titleMedium),
+        Text("titleSmall", style: Theme.of(context).textTheme.titleSmall),
+        FilledButton(onPressed: () {}, child: Text("filledButton")),
+        ElevatedButton(onPressed: () {}, child: Text("elevatedButton")),
+        TextButton(onPressed: () {}, child: Text("textButton")),
+        IconButton(onPressed: () {}, icon: Icon(Icons.abc)),
+        Switch(value: true, onChanged: (v) {})
+      ],
+    );
+  }
+}
