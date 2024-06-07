2bb0c60eed2b911c47fce85c4fb2d375f36a99b3
fowardslash <quangrain2014@gmail.com>
2024-06-07 17:00:01 +0700

    bottom navigation

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,6 @@
 import 'package:app_well_mate/components/shotcut.dart';
 import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,30 +12,70 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://images-ext-1.discordapp.net/external/GzOumY3Ty-mCaQNSxtMOVR5BPLNstdlilADmc80Wfm8/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/515061888258670602/69eaf1984e071ba575fe531b70b200c3.png?format=webp&quality=lossless&width=452&height=452"),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Xin chào,",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                Text("Trí Quang", style: Theme.of(context).textTheme.bodyMedium)
+              ],
+            )
+          ],
+        ),
+        
+      ),
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("bodyLarge", style: Theme.of(context).textTheme.bodyLarge),
-          Text("bodyMedium", style: Theme.of(context).textTheme.bodyMedium),
-          Text("bodySmall", style: Theme.of(context).textTheme.bodySmall),
-          Text("displayLarge", style: Theme.of(context).textTheme.displayLarge),
-          Text("displayMedium", style: Theme.of(context).textTheme.displayMedium),
-          Text("displaySmall", style: Theme.of(context).textTheme.displaySmall),
-          Text("headlineLarge", style: Theme.of(context).textTheme.headlineLarge),
-          Text("headlineMedium", style: Theme.of(context).textTheme.headlineMedium),
-          Text("headlineSmall", style: Theme.of(context).textTheme.headlineSmall),
-          Text("labelLarge", style: Theme.of(context).textTheme.labelLarge),
-          Text("labelMedium", style: Theme.of(context).textTheme.labelMedium),
-          Text("labelSmall", style: Theme.of(context).textTheme.labelSmall),
-          Text("titleLarge - AppBar", style: Theme.of(context).textTheme.titleLarge),
-          Text("titleMedium", style: Theme.of(context).textTheme.titleMedium),
-          Text("titleSmall", style: Theme.of(context).textTheme.titleSmall),
-          FilledButton(onPressed: () {}, child: Text("filledButton")),
-          ElevatedButton(onPressed: () {}, child: Text("elevatedButton")),
-          TextButton(onPressed: () {}, child: Text("textButton")),
-          IconButton(onPressed: () {}, icon: Icon(Icons.abc)),
-          Switch(value: true, onChanged: (v) {}),
-          Shortcut()
+          Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text("Tác vụ nhanh")),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                    child: Shortcut(
+                  icon: Icons.monitor_weight_outlined,
+                  text: "Tính BMI",
+                )),
+                Expanded(
+                    child: Shortcut(
+                  icon: Icons.leaderboard_outlined,
+                  text: "Theo dõi",
+                )),
+                Expanded(
+                    child: Shortcut(
+                  icon: Icons.camera_alt_outlined,
+                  text: "Quét mã QR",
+                )),
+                Expanded(
+                    child: Shortcut(
+                  icon: Symbols.pill,
+                  text: "Xem toa thuốc",
+                )),
+              ],
+            ),
+          ),
+          Divider(
+            indent: 20,
+            endIndent: 20,
+          )
         ],
       ),
     );
