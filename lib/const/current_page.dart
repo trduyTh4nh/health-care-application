16188daba8bb17cdb2d37ff48fbb4a9cfa028816
NguyenDuy <97612815+racingboy2@users.noreply.github.com>
2024-06-13 12:58:04 +0700

    ontap trong shortcut of quick action

--- lib/const/current_page.dart
+++ lib/const/current_page.dart
@@ -2,4 +2,4 @@ import 'package:app_well_mate/main.dart';
 import 'package:app_well_mate/screen/admin/admin_page.dart';
 import 'package:flutter/material.dart';
 
-Widget currentpage = const AdminPage();
+Widget currentpage = const AppPage();
---
+++ lib/screen/quick_action/bmi_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BmiPage extends StatefulWidget {
+  const BmiPage({super.key});
+
+  @override
+  State<BmiPage> createState() => _BmiPageState();
+}
+
+class _BmiPageState extends State<BmiPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tính BMI"),
+      ),
+    );
+  }
+}
