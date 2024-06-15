77e09c51c45a5387e4b3f6a91c6b56707953a99a
Phat Ngo <phatngo1908@gmail.com>
2024-06-15 14:06:42 +0700

    policy page
    update

--- lib/const/current_page.dart
+++ lib/const/current_page.dart
@@ -1,6 +1,7 @@
 import 'package:app_well_mate/main.dart';
 import 'package:app_well_mate/screen/changeRePassword.dart';
 import 'package:app_well_mate/screen/drugDetails.dart';
+import 'package:app_well_mate/screen/policy.dart';
 import 'package:flutter/material.dart';
 
-Widget currentpage = const Changerepassword();
+Widget currentpage = const Policy();
