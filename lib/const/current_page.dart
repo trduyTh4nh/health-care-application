75dacbdd636a483a379507a7df907f0243874d8e
Phat Ngo <phatngo1908@gmail.com>
2024-06-15 13:55:28 +0700

    screen changeRepass and drugDetails
    update changeRepass and Create drugDetails

--- lib/const/current_page.dart
+++ lib/const/current_page.dart
@@ -1,4 +1,6 @@
 import 'package:app_well_mate/main.dart';
+import 'package:app_well_mate/screen/changeRePassword.dart';
+import 'package:app_well_mate/screen/drugDetails.dart';
 import 'package:flutter/material.dart';
 
-Widget currentpage = const AppPage();
+Widget currentpage = const Changerepassword();
