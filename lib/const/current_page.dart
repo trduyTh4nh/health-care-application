f4a7e1b6a1890e39eb581a485d20b59118b3efae
Phat Ngo <phatngo1908@gmail.com>
2024-06-21 14:46:23 +0700

    temp

--- lib/const/current_page.dart
+++ lib/const/current_page.dart
@@ -1,10 +1,14 @@
+import 'package:app_well_mate/components/drug_super_detail_item.dart';
 import 'package:app_well_mate/main.dart';
+import 'package:app_well_mate/model/notification_model.dart';
+import 'package:app_well_mate/model/notification_model.dart';
 import 'package:app_well_mate/screen/admin/admin_page.dart';
 import 'package:app_well_mate/screen/admin/hospital_management_page.dart';
 import 'package:app_well_mate/screen/admin/user_management_page.dart';
 import 'package:app_well_mate/screen/changeRePassword.dart';
 import 'package:app_well_mate/screen/drug_super_detail.dart';
+import 'package:app_well_mate/screen/home.dart';
 import 'package:app_well_mate/screen/quick_action/bmi_page.dart';
 import 'package:flutter/material.dart';
 
-Widget currentpage = const Changerepassword();
+Widget currentpage = DrugSuperDetail();
--- lib/screen/drug_super_detail.dart
+++ lib/screen/drug_super_detail.dart
@@ -1,3 +1,6 @@
+import 'package:app_well_mate/components/drug_super_detail_item.dart';
+import 'package:app_well_mate/model/notification_model.dart';
+import 'package:app_well_mate/utils/app.colors.dart';
 import 'package:flutter/material.dart';
 
 class DrugSuperDetail extends StatefulWidget {
@@ -7,18 +10,121 @@ class DrugSuperDetail extends StatefulWidget {
   State<DrugSuperDetail> createState() => _DrugSuperDetailState();
 }
 
-class _DrugSuperDetailState extends State<DrugSuperDetail> {
+class _DrugSuperDetailState extends State<DrugSuperDetail>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  List<NotificationModel> notifications = [];
+  List<NotificationModel> _listRed = [];
+  List<NotificationModel> _listBlue = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    if (listNotification != null) {
+      notifications = listNotification;
+
+      _listRed = notifications
+          .where((e) =>
+              (e.priority! == 'overdue' || e.priority! == 'runoutof') &&
+              checkExpire(e.time!))
+          .toList();
+      // sort
+      _listRed.sort((a, b) => b.time!.compareTo(a.time!));
+
+      _listBlue = notifications
+          .where((e) => (!_listRed.contains(e)) && checkExpire(e.time!))
+          .toList();
+      _listBlue.sort((a, b) => b.time!.compareTo(a.time!));
+    }
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: const Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Text("03/02/2003 - Chi tiết")],
+        title: const Text('03/02/2023 - Chi Tiết'),
+        bottom: TabBar(
+          controller: _tabController,
+          labelColor: AppColors.primaryColor,
+          indicatorColor: AppColors.primaryColor,
+          unselectedLabelColor: Colors.black,
+          indicatorWeight: 2.0,
+          tabs: const [
+            Tab(text: 'Thuốc chưa uống'),
+            Tab(text: 'Thuốc đã uống'),
+          ],
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.only(left: 24, right: 24),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Text(
+                'Quá hạn',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return Column();
+                },
+                childCount: _listRed.length,
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  var now = DateTime.now();
+                  var difference = now.difference(_listBlue[index].time!);
+                  //var timeOfNotifi = now.difference(_listBlue[index].time!);
+
+                  List<NotificationModel> arrRecently = [];
+                  List<NotificationModel> arrYesterday = [];
+                  List<NotificationModel> arrFurther = [];
+
+                  for (var notification in _listBlue) {
+                    var timeOfNotifi = now.difference(notification.time!);
+
+                    if (timeOfNotifi.inMinutes < 60 ||
+                        timeOfNotifi.inHours < 24) {
+                      arrRecently.add(notification);
+                    } else if (timeOfNotifi.inDays < 7) {
+                      arrYesterday.add(notification);
+                    } else {
+                      arrFurther.add(notification);
+                    }
+                  }
+
+                  List<Widget> children = [];
+
+                  return Column(
+                    children: children,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                  );
+                },
+                childCount: 1,
+              ),
+            )
+          ],
         ),
       ),
     );
   }
+}
+
+bool checkExpire(DateTime time) {
+  var now = DateTime.now();
+  var difference = now.difference(time);
+
+  return difference.inDays <= 100 ? true : false;
 }
