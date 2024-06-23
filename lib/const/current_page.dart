257ff5f331d0c54679482dc453d3b7d80ae57126
Phat Ngo <phatngo1908@gmail.com>
2024-06-23 18:55:19 +0700

    fat push code

--- lib/const/current_page.dart
+++ lib/const/current_page.dart
@@ -4,6 +4,7 @@ import 'package:app_well_mate/model/notification_model.dart';
 import 'package:app_well_mate/model/notification_model.dart';
 import 'package:app_well_mate/screen/admin/admin_page.dart';
 import 'package:app_well_mate/screen/drug/medicine_order/medicines_order_main.dart';
+import 'package:app_well_mate/screen/edit_infomation_user.dart';
 import 'package:app_well_mate/screen/home.dart';
 import 'package:app_well_mate/screen/login.dart';
 import 'package:app_well_mate/screen/admin/hospital_management_page.dart';
@@ -14,5 +15,4 @@ import 'package:app_well_mate/screen/home.dart';
 import 'package:app_well_mate/screen/quick_action/bmi_page.dart';
 import 'package:flutter/material.dart';
 
-
-Widget currentpage = const AdminPage();
+Widget currentpage = const EditInfomationUser();
