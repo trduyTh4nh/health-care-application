7e72ad6473c14dbbac006eb592bf9849d38e3f4f
NguyenDuy <97612815+racingboy2@users.noreply.github.com>
2024-06-23 17:41:47 +0700

    all drugs page

--- lib/screen/medication.dart
+++ lib/screen/medication.dart
@@ -1,6 +1,7 @@
 import 'package:app_well_mate/components/medication_fab.dart';
 import 'package:app_well_mate/const/color_scheme.dart';
 import 'package:app_well_mate/screen/drug/add_drug.dart';
+import 'package:app_well_mate/screen/drug/schedule_pages/all_drug.dart';
 import 'package:app_well_mate/screen/drug/schedule_pages/drug_done.dart';
 import 'package:app_well_mate/screen/drug/schedule_pages/drug_today.dart';
 import 'package:flutter/material.dart';
@@ -18,29 +19,29 @@ class _MedicationPageState extends State<MedicationPage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2,
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading: false,
           title: const Text("Lịch uống thuốc"),
-          bottom: TabBar(
-            dividerColor: AppColor.gray,
-            tabs: const [
-            Tab(text: "Thuốc chưa uống",),
-            Tab(text: "Thuốc đã uống",),
+          bottom: TabBar(dividerColor: AppColor.gray, tabs: const [
+            Tab(
+              text: "Thuốc chưa uống",
+            ),
+            Tab(
+              text: "Thuốc đã uống",
+            ),
             Tab(text: "Tất cả thuốc")
           ]),
         ),
         floatingActionButton: const Column(
           mainAxisSize: MainAxisSize.min,
-          children: [
-            MedicationFab()
-          ],
+          children: [MedicationFab()],
         ),
         body: const TabBarView(children: [
           DrugToday(),
           DrugDone(),
-          DrugDone()
+          AllDrug(),
         ]),
       ),
     );
