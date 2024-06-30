605fda2dce6cb1e8bd6fcd194610e080b76975d4
Duy Thanh <tttranduy999@gmail.com>
2024-06-30 19:12:19 +0700

    push

--- lib/const/current_page.dart
+++ lib/const/current_page.dart
@@ -2,6 +2,7 @@
 import 'package:app_well_mate/main.dart';
 // ignore: unused_import
 import 'package:app_well_mate/screen/admin/admin_page.dart';
+import 'package:app_well_mate/screen/transaction_detail.dart';
 import 'package:flutter/material.dart';
 
 Widget currentpage = const AppPage();
