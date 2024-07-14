d64a79181e53af5bf08791e3a219811eb1d524b2
NguyenDuy <97612815+racingboy2@users.noreply.github.com>
2024-07-14 18:50:43 +0700

    dangky use api

---
+++ lib/components/snack_bart.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void showCustomSnackBar(BuildContext context, String message) {
+  final snackBar = SnackBar(
+    content: Text(message),
+  );
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
