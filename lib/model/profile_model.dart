a631a280e013f3ec7588d3a30d1888856c399f6c
Phat Ngo <phatngo1908@gmail.com>
2024-07-20 14:36:16 +0700

    update profile api co hinh
    update code

--- lib/model/profile_model.dart
+++ lib/model/profile_model.dart
@@ -2,25 +2,27 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'json/profile_model.g.dart';
 
-@JsonSerializable()
+// @JsonSerializable()
 class ProfileModel {
   int? idProfile;
-  String? name;
-  int? height;  
+
+  int? height;
   int? weight;
-  DateTime? age;
+  int? age;
   String? gender;
   String? address;
   int? idUser; // khóa ngoại
+  String? avtUser;
   ProfileModel(
       {this.idProfile,
-      this.name,
       this.height,
       this.weight,
       this.age,
       this.gender,
       this.address,
-      this.idUser});
-  factory ProfileModel.fromJson(Map<String, dynamic> json) => _$ProfileModelFromJson(json);
+      this.idUser,
+      this.avtUser});
+  factory ProfileModel.fromJson(Map<String, dynamic> json) =>
+      _$ProfileModelFromJson(json);
   Map<String, dynamic> toJson() => _$ProfileModelToJson(this);
 }
