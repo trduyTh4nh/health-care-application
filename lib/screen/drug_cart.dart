01c83cc3ae65decdd62ba0e60635cc514a0a196c
NguyenDuy <97612815+racingboy2@users.noreply.github.com>
2024-07-24 14:59:48 +0700

    fix giao dien

--- lib/screen/drug_cart.dart
+++ lib/screen/drug_cart.dart
@@ -1,9 +1,11 @@
 import 'package:app_well_mate/components/info_component.dart';
 import 'package:app_well_mate/components/item_drug_cart_detail.dart';
+import 'package:app_well_mate/main.dart';
 import 'package:app_well_mate/providers/cart_page_provider.dart';
 import 'package:app_well_mate/utils/util.dart';
 import 'package:flutter/material.dart';
 import 'package:app_well_mate/screen/drug/medicine_order/medicines_order_main.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:material_symbols_icons/symbols.dart';
 import 'package:provider/provider.dart';
 
@@ -25,93 +27,116 @@ class _CartPageState extends State<CartPage> {
     });
   }
 
+  void setLoading(bool loading) {
+    setState(() {
+      isLoading = loading;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Giỏ hàng'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Provider.of<CartPageProvider>(context, listen: false)
-                    .toggleAllChecks();
-              },
-              icon: const Badge(
-                child: Icon(Icons.checklist),
-                smallSize: 0,
-                largeSize: 0,
-              )),
-        ],
-      ),
-      body: Consumer<CartPageProvider>(
-        builder: (context, value, child) {
-          return Column(
-            children: [
-              Expanded(
-                child: value.listDrugCart.isEmpty
-                    ? const Center(
-                        child: ErrorInfo(
-                        title: "Không có sản phẩm nào trong giỏ hàng",
-                        subtitle: "Vui lòng thêm một sản phẩm vào giỏ",
-                        icon: Symbols.shopping_cart_off,
-                      ))
-                    : ListView.builder(
-                        itemCount: value.listDrugCart.length,
-                        itemBuilder: (context, index) {
-                          var item = value.listDrugCart[index];
-                          return itemDrugCartDetail(
-                            item: item,
-                            value: value,
-                            index: index,
-                          );
-                        },
-                      ),
-              ),
-              //           //PHAN NAY TINH TONG TIEN
-              SizedBox(
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 16),
-                      Text(
-                        'Tổng tiền: ',
-                        style: Theme.of(context).textTheme.bodyLarge,
-                      ),
-                      Text(
-                        convertCurrency(value.totalPrice),
-                        style: Theme.of(context).textTheme.displayMedium,
-                      ),
-                      Align(
-                        alignment: Alignment.center,
-                        child: SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            onPressed: value.listChecked.isEmpty
-                                ? null
-                                : () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            const MedicinesOrder(),
-                                      ),
-                                    );
-                                  },
-                            child: const Text('Mua thuốc'),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            title: const Text('Giỏ hàng'),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    Provider.of<CartPageProvider>(context, listen: false)
+                        .toggleAllChecks();
+                  },
+                  icon: const Badge(
+                    child: Icon(Icons.checklist),
+                    smallSize: 0,
+                    largeSize: 0,
+                  )),
             ],
-          );
-        },
-      ),
+          ),
+          body: Consumer<CartPageProvider>(
+            builder: (context, value, child) {
+              return Column(
+                children: [
+                  Expanded(
+                    child: value.listDrugCart.isEmpty
+                        ? const Center(
+                            child: ErrorInfo(
+                            title: "Không có sản phẩm nào trong giỏ hàng",
+                            subtitle: "Vui lòng thêm một sản phẩm vào giỏ",
+                            icon: Symbols.shopping_cart_off,
+                          ))
+                        : ListView.builder(
+                            itemCount: value.listDrugCart.length,
+                            itemBuilder: (context, index) {
+                              var item = value.listDrugCart[index];
+                              return itemDrugCartDetail(
+                                  item: item,
+                                  value: value,
+                                  index: index,
+                                  setLoading: setLoading);
+                            },
+                          ),
+                  ),
+                  //           //PHAN NAY TINH TONG TIEN
+                  SizedBox(
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 16),
+                          Text(
+                            'Tổng tiền: ',
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                          Text(
+                            convertCurrency(value.totalPrice),
+                            style: Theme.of(context).textTheme.displayMedium,
+                          ),
+                          Align(
+                            alignment: Alignment.center,
+                            child: SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                onPressed: value.listChecked.isEmpty
+                                    ? null
+                                    : () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                const MedicinesOrder(),
+                                          ),
+                                        );
+                                      },
+                                child: const Text('Mua thuốc'),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              );
+            },
+          ),
+        ),
+        isLoading
+            ? Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.grey.withOpacity(0.5),
+                child: Center(
+                  child: LoadingAnimationWidget.flickr(
+                    leftDotColor: colorScheme.primary,
+                    rightDotColor: colorScheme.error,
+                    size: 48,
+                  ),
+                ))
+            : const SizedBox()
+      ],
     );
   }
 }
